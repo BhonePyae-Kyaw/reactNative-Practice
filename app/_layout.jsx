6499bb655c895d9d1f599f20afb9b0e52871a53a
Bhone Pyae Kyaw <137211983+BhonePyae-Kyaw@users.noreply.github.com>
2024-04-27 12:44:29 +0700

    set up update: solve the problem

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Aora says "Hello World!".</Text>
+      <Text>Aora says "Hello World!". This is working.....</Text>
       <StatusBar style="auto" />
     </View>
   );
